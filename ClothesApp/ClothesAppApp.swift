225866b488b1441f7edd7fa202ddc51ecdc37101
Mustafa Emir Ata <mustafaemirata@outlook.com>
2026-03-15 03:43:45 +0300

    Initial Commit

---
+++ ClothesApp/ClothesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClothesAppApp.swift
+//  ClothesApp
+//
+//  Created by Mustafa Emir Ata on 15.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct ClothesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClothesAppTests/ClothesAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClothesAppTests.swift
+//  ClothesAppTests
+//
+//  Created by Mustafa Emir Ata on 15.03.2026.
+//
+
+import Testing
+@testable import ClothesApp
+
+struct ClothesAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
